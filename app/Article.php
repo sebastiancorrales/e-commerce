5ac9e1363fc1aa526c176beb44ffeb6201778a36
sebastiancorrales <sebastiancorrales477@gmail.com>
2020-07-08 16:30:56 -0500

    Article, Sucursal

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'categoria',
+        'nombre', 
+        'descripcion',
+        'precio',
+        'valor_envio',
+        'fecha_envio',
+        'direccion_envio',
+        'direccion_destino',
+        'sucursal'
+    ];
+    public function sucursal(){
+        return $this->belongsTo('App\Sucursal');
+    }
 }
